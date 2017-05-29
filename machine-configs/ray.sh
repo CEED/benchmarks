49b2bdea8d1ae8ff2e546d4ce79d3ca7aea3505d
Veselin Dobrev <dobrev@llnl.gov>
2017-05-29 00:30:46 -0700

    Add config file for the early access system ray.llnl.gov.

---
+++ machine-configs/ray.sh
@@ -0,0 +1,72 @@
+# This file is part of CEED. For more details, see exascaleproject.org.
+
+
+function setup_xlc()
+{
+   MPICC=mpixlc
+   MPICXX=mpixlC
+   mpi_info_flag="-qversion=verbose"
+
+   CFLAGS="-O3 -qarch=auto -qcache=auto -qhot -qtune=auto"
+   TEST_EXTRA_CFLAGS="-O5"
+   # TEST_EXTRA_CFLAGS+=" -std=c++11 -qreport"
+}
+
+
+function setup_gcc()
+{
+   # GCC 4.9.3 is the default GCC as of 2017-05-28.
+
+   MPICC=mpigcc
+   MPICXX=mpig++
+   mpi_info_flag="--version"
+
+   CFLAGS="-O3 -mcpu=native -mtune=native"
+   TEST_EXTRA_CFLAGS=""
+   # TEST_EXTRA_CFLAGS+=" -std=c++11 -fdump-tree-optimized-blocks"
+}
+
+
+function setup_clang()
+{
+   # Clang coral compilers: clang-coral-2017.05.19 as of 2017-05-29.
+
+   MPICC=mpiclang
+   MPICXX=mpiclang++
+   mpi_info_flag="--version"
+
+   CFLAGS="-O3 -mcpu=native -mtune=native"
+   TEST_EXTRA_CFLAGS="-fcolor-diagnostics -fvectorize"
+   TEST_EXTRA_CFLAGS+=" -fslp-vectorize -fslp-vectorize-aggressive"
+   TEST_EXTRA_CFLAGS+=" -ffp-contract=fast"
+}
+
+
+function set_mpi_options()
+{
+   MPIEXEC_OPTS="-npernode $num_proc_node"
+   # Q: Is bind.sh equivalent to the mpirun option '-bind-to core'?
+   MPIEXEC_OPTS="-bind-to core $MPIEXEC_OPTS"
+   # MPIEXEC_OPTS+=" -report-bindings"
+   # echo "$LSB_HOSTS" > hostfile
+   # MPIEXEC_OPTS+=" -hostfile hostfile"
+   if [ "$num_proc_node" -gt "20" ]; then
+      MPIEXEC_OPTS="-oversubscribe $MPIEXEC_OPTS"
+   fi
+   local BSUB_OPTS="-q pdebug -G guests -n $((num_nodes*20)) -I"
+   MPIEXEC_OPTS="$BSUB_OPTS mpirun $MPIEXEC_OPTS"
+}
+
+
+valid_compilers="xlc gcc clang"
+num_proc_build=10
+num_proc_run=${num_proc_run:-20}
+num_proc_node=${num_proc_node:-20}
+memory_per_node=256
+
+# Optional (default): MPIEXEC (mpirun), MPIEXEC_OPTS (), MPIEXEC_NP (-np)
+# bind_sh=mpibind
+# bind_sh=bind.sh
+# MPIEXEC=jsrun (under dev)
+MPIEXEC="bsub"
+MPIEXEC_NP="-n"
