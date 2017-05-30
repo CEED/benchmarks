7373f50858388dc90914c71d4c8546073da7fc54
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-05-30 12:40:23 -0600

    Add the nek5000 clone and build script
    Build script only builds the tools needed for the bakeoff problems:
    genmap and genbox

---
+++ package-builders/nek5000.sh
@@ -0,0 +1,53 @@
+# This file is part of CEED. For more details, see exascaleproject.org.
+
+# Clone Nek5000.
+
+if [[ -z "$pkg_sources_dir" ]]; then
+   echo "This script ($0) should not be called directly. Stop."
+   return 1
+fi
+if [[ -z "$OUT_DIR" ]]; then
+   echo "The variable 'OUT_DIR' is not set. Stop."
+   return 1
+fi
+pkg_src_dir="Nek5000"
+NEK5K_SOURCE_DIR="$pkg_sources_dir/$pkg_src_dir"
+pkg_bld_dir="$OUT_DIR/nek5000"
+MFEM_DIR="$pkg_bld_dir"
+pkg="NEK5K"
+
+
+function nek5k_clone()
+{
+   pkg_repo_list=("git@github.com:Nek5000/Nek5000.git"
+                  "https://github.com/Nek5000/Nek5000.git")
+   pkg_git_branch="master"
+   cd "$pkg_sources_dir" || return 1
+   if [[ -d "$pkg_src_dir" ]]; then
+      update_git_package
+      return
+   fi
+   for pkg_repo in "${pkg_repo_list[@]}"; do
+      echo "Cloning $pkg from $pkg_repo ..."
+      git clone "$pkg_repo" "$pkg_src_dir" && return 0
+   done
+   echo "Could not successfully clone $pkg. Stop."
+   return 1
+}
+
+function nek5k_build()
+{
+## Just build the requited tools: genbox and genmap
+   cd $NEK5K_SOURCE_DIR
+   echo `pwd`
+   echo "Okay"
+   cd tools
+   ./maketools genmap
+   ./maketools genbox
+   return 0
+}
+
+function build_package()
+{
+   nek5k_clone && nek5k_build
+}
