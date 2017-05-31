83c6684eaec96caf294d554285ae0d68903a77f9
Thilina Rathnayake <thilinarmtb@gmail.com>
2017-05-30 18:07:37 -0600

    Now the tests are generated inside build directory
    TODO:
      * use the correct build parameters in makenek file
      * Run the tests
      * get the output
      * polish the scripts

--- tests/nek5000_bps/bp1/bp1.sh
+++ tests/nek5000_bps/bp1/bp1.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-export BP_ROOT=`pwd`/..
-export BENCH_ROOT="$BP_ROOT/../.."
+function build_and_run_tests()
+{
+   export BP_ROOT=`pwd`/tests/nek5000_bps
+   export BENCH_ROOT=`pwd`
 
-# Generate the boxes
-cd $BP_ROOT/boxes
-./boxes.sh
-cd $BP_ROOT/bp1
+   # Generate the boxes
+   cd $BP_ROOT/boxes
+   ./boxes.sh
+   cd $OUT_DIR/bp1
 
-# Setup the sin version of the bp
-mkdir sin
-cd sin
+   # Setup the sin version of the bp
+   mkdir sin
+   cd sin
 
-for i in `seq 2 1 4`
-do
+   for i in `seq 2 1 4`
+   do
 
-mkdir lx$i
-cp -r $BP_ROOT/boxes/b?? $BP_ROOT/SIZE ../zsin.usr lx$i/
+      mkdir lx$i
+      cp -r $BP_ROOT/boxes/b?? $BP_ROOT/SIZE $BP_ROOT/bp1/zsin.usr lx$i/
 
-# Set lx1 in SIZE file
-sed -i "s/lx1=[0-9]*/lx1=${i}/" lx$i/SIZE
+      # Set lx1 in SIZE file
+      sed -i "s/lx1=[0-9]*/lx1=${i}/" lx$i/SIZE
 
-# Make the executable
-cd lx$i
-$BP_ROOT/makenek zsin
-cd ..
+      # Make the executable
+      cd lx$i
+      $BP_ROOT/makenek zsin
+      cd ..
 
-done
+   done
+
+   return 0
+}
