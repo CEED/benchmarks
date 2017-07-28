7f59bf64b9cfb24dd215dedfb80e81621870bb75
Veselin Dobrev <dobrev@llnl.gov>
2017-07-27 19:07:37 -0700

    In tests/mfem_bps, add support for number of processors that is not a power of 2. For example, to run on 36 processors, with up to 100*2^20 dofs, one can use:
     ~/bench/tests/mfem_bps> ../../go.sh -c quartz -m gcc -r bp1_v1.sh \
                                -n 36 -p 36 base_nxyz='"3 3 4"' \
                                max_dofs='$((100*(2**20)))'

--- tests/mfem_bps/bp1_v1.sh
+++ tests/mfem_bps/bp1_v1.sh
@@ -69,10 +69,11 @@ function build_tests()
 
 function split3_power2()
 {
-   local number="$2" outvar="$1" n1= n2= n3=
-   (( n3 = 2**(number/3) ))
-   (( n2 = n3*(2**(number%3>1?1:0)) ))
-   (( n1 = n3*(2**(number%3>0?1:0)) ))
+   local outvar="$1" number="$2" n1="$3" n2="$4" n3="$5" nn=
+   (( nn = 2**(number/3) ))
+   (( n1 = n1*nn*(2**(number%3>0?1:0)) ))
+   (( n2 = n2*nn*(2**(number%3>1?1:0)) ))
+   (( n3 = n3*nn ))
    eval "$outvar=($n1 $n2 $n3)"
 }
 
@@ -141,8 +142,6 @@ vec_layout=${vec_layout:-}
 # test id:     0   1   2   3   4   5   6   7   8   9
 sol_p_list=(   1   2   3   4   5   6   7   8   1   2)
 ir_order_list=(0   0   0   0   0   0   0   0   3   5)
-mesh_s_list=( 15  15  16  15  14  13  13  12  15  15)
-par_ref_list=( 2   1   0   0   0   0   0   0   2   1) # set below
 enabled_tests_def="0   1   2   3   4   5   6   7   8   9"
 # enabled_tests_def="1   2   3   4   5   6   7   8"   # for bp3 on vulcan + xlc
 # enabled_tests_def="0"
@@ -150,43 +149,28 @@ enabled_tests="${enabled_tests:-$enabled_tests_def}"
 ser_ref=0
 mesh_s_reduction_base=0     # used in run_tests_if_enabled()
 mesh_s_reduction_limit=30   # used in run_tests_if_enabled()
-mesh_s_max=29 # s=30 causes overflow in number of faces/edges
+mesh_max_elem=$(( 2**29 )) # 2^30 causes overflow in number of faces/edges
 max_approx_dofs=$((3*(2**20)*num_nodes))
+max_approx_dofs="${max_dofs:-$max_approx_dofs}"
+echo "max_approx_dofs: $max_approx_dofs"
+base_nxyz=(${base_nxyz[@]:-1 1 1})
+base_n=$(( base_nxyz[0]*base_nxyz[1]*base_nxyz[2] ))
 use_mpi_wtime=yes # leave empty for 'no'
 rebuild_tests=no
 
-local n=$num_proc_run
+local n=$(( num_proc_run/base_n ))
 proc_t=0
 while (( n >=2 )); do
    ((proc_t=proc_t+1))
    ((n=n/2))
 done
-(( num_proc_run == 2**proc_t )) || {
+(( num_proc_run == base_n*(2**proc_t) )) || {
    echo "Invalid number of processors: $num_proc_run"
-   echo "The number of processors must be a power of 2."
+   echo "The number of processors must be $base_n times a power of 2."
    return 1
 }
-split3_power2 proc_nxyz $proc_t
+split3_power2 proc_nxyz $proc_t "${base_nxyz[@]}"
 test_extra_args=(-c "${proc_nxyz[*]}")
-
-case $num_nodes in
-   1)
-      par_ref_list=( 2   1   0   0   0   0   0   0   2   1)
-      ;;
-   8)
-      par_ref_list=( 3   2   1   1   1   1   1   1   3   2)
-      ;;
-   64)
-      par_ref_list=( 4   3   2   2   2   2   2   2   4   3)
-      ;;
-   512)
-      par_ref_list=( 5   4   3   3   3   3   3   3   5   4)
-      ;;
-   *)
-      printf "\nNumber of nodes = $num_nodes is not configured!\n"
-      return 1
-      ;;
-esac
 
 return 0
 
@@ -197,20 +181,29 @@ function set_test_params()
    [[ -n "$1" ]] && {
       sol_p="${sol_p_list[$1]}"
       ir_order="${ir_order_list[$1]}"
-      mesh_s="${mesh_s_list[$1]}"
+      local max_elems=$(( max_approx_dofs/((sol_p**3)*vdim) ))
+      mesh_s="0"
+      while (( base_n*(2**(mesh_s+1)) <= max_elems )); do
+         (( mesh_s = mesh_s + 1 ))
+      done
       (( mesh_s = mesh_s + mesh_s_shift ))
-      par_ref="${par_ref_list[$1]}"
+      par_ref="0"
+      while (( mesh_s >= 3 )); do
+         (( mesh_s = mesh_s - 3 ))
+         (( par_ref = par_ref + 1 ))
+      done
    }
    # echo "proc_t = $proc_t, mesh_s = $mesh_s, ser_ref = $ser_ref"
    if (( mesh_s + 3*(ser_ref+par_ref) < 0 )); then
       # invalid number of elements
       return 1
    fi
-   if (( mesh_s + 3*(ser_ref+par_ref) > mesh_s_max )); then
+   local num_elem=$(( base_n*(2**(mesh_s+3*(ser_ref+par_ref))) ))
+   if (( num_elem > mesh_max_elem )); then
       # too many elements
       return 1
    fi
-   local approx_dofs=$(( 2**(mesh_s+3*(ser_ref+par_ref))*(sol_p**3)*vdim ))
+   local approx_dofs=$(( num_elem*(sol_p**3)*vdim ))
    if (( approx_dofs > max_approx_dofs )); then
       # too many dofs
       return 1
@@ -235,7 +228,7 @@ function set_test_params()
          suffix="_VN$suffix" || \
          suffix="_VV$suffix"
    }
-   split3_power2 mesh_nxyz $mesh_s
+   split3_power2 mesh_nxyz $mesh_s "${base_nxyz[@]}"
    make_mesh_file
    mesh_opt="-m $mesh_file"
 }
@@ -251,7 +244,7 @@ function run_tests_if_enabled()
             set_test_params $test_id && run_test || {
                if (( mesh_s+3*(ser_ref+par_ref) >= 0 )); then
                   printf "Skipped test #$test_id with number of mesh elements"
-                  echo " = $(( 2**(mesh_s+3*(ser_ref+par_ref)) ))."
+                  echo " = $(( base_n*(2**(mesh_s+3*(ser_ref+par_ref))) ))."
                fi
             }
          done
