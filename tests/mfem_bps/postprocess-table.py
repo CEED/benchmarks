d17a5ad1d6a05154b345a5f1b595759da3aea368
Veselin Dobrev <dobrev@llnl.gov>
2018-11-16 18:02:05 -0800

    In tests/mfem_bps/postprocess-table.py, add a column with the problem size.

--- tests/mfem_bps/postprocess-table.py
+++ tests/mfem_bps/postprocess-table.py
@@ -27,15 +27,16 @@ mf={'a': 'A', 'm': 'PA'}
 set1=sorted(
    [(run['order'],run['quadrature-pts'],mf[run['action-type'][0]],
      hpc[run['std | hpc'][0]],run['compiler'],run['num-procs'],
+     run['num-unknowns'],
      run['assembly-dps']/1e6,run['cg-iteration-dps']/1e6,
      1.*run['num-elem']*run['quadrature-pts']/run['num-unknowns'])
     for run in runs])
 
 out.write('''\
-    | quad | asse | std |  comp  |     | assembly dps | cg-iter dps | qpts per
-  p |  pts | mbly | hpc |  iler  |  np |   millions   |   millions  |  unknown
-----+------+------+-----+--------+-----+--------------+-------------+---------
+    | quad | asse | std |  comp  |     | number of | assembly dps | cg-iter dps | qpts per
+  p |  pts | mbly | hpc |  iler  |  np |  unknowns |   millions   |   millions  |  unknown
+----+------+------+-----+--------+-----+-----------+--------------+-------------+---------
 ''')
-line_fmt=' %2i | %4i |  %2s  | %s | %6s | %3i | %11.6f  | %11.6f | %7.4f\n'
+line_fmt=' %2i | %4i |  %2s  | %s | %6s | %3i | %9i | %11.6f  | %11.6f | %7.4f\n'
 for run in set1:
    out.write(line_fmt%run)
