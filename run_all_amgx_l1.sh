4affbba633845c856f6c675638473e8d27d1adb9
Arturo Vargas <artv3.89@gmail.com>
2020-12-23 09:54:44 -0800

    minor edits to job scripts

--- run_all_amgx_l1.sh
+++ run_all_amgx_l1.sh
@@ -1,11 +1,5 @@
-#Using Block JACOBI SMOOTHER
-# ./go.sh -c lassen -m xlc -r tests/mfem_amgx/ex1p_amgx_solver.sh num_proc_build=36  >> amgx_solver &&
-# ./go.sh -c lassen -m xlc -r tests/mfem_amgx/ex1p_amgx_precon.sh num_proc_build=36  >> amgx_precon &&
-# ./go.sh -c lassen -m xlc -r tests/mfem_amgx/ex1p_amgx_agg_precon.sh num_proc_build=36  >> amgx_agg_precon &&
-# ./go.sh -c lassen -m xlc -r tests/mfem_amgx/ex1p_amgx_agg_solver.sh num_proc_build=36  >> amgx_agg_solver &&
-
-#Using L1 JACOBI SMOOTHER
-./go.sh -c lassen -m xlc -r tests/mfem_amgx/ex1p_amgx_solver_l1_jacobi.sh num_proc_build=36  >> amgx_solver_l1_jacobi &&
-./go.sh -c lassen -m xlc -r tests/mfem_amgx/ex1p_amgx_precon_l1_jacobi.sh num_proc_build=36  >> amgx_precon_l1_jacobi &&
-./go.sh -c lassen -m xlc -r tests/mfem_amgx/ex1p_amgx_agg_precon_l1_jacobi.sh num_proc_build=36  >> amgx_agg_precon_l1_jacobi &&
-./go.sh -c lassen -m xlc -r tests/mfem_amgx/ex1p_amgx_agg_solver_l1_jacobi.sh num_proc_build=36  >> amgx_agg_solver_l1_jacobi
+#Using L1 Jacobi Smoother
+./go.sh -c lassen -m xlc -r tests/mfem_amgx/ex1p_amgx_solver_l1_jacobi.sh num_proc_build=36  >> amgx_solver_l1_jacobi.txt &&
+./go.sh -c lassen -m xlc -r tests/mfem_amgx/ex1p_amgx_precon_l1_jacobi.sh num_proc_build=36  >> amgx_precon_l1_jacobi.txt &&
+./go.sh -c lassen -m xlc -r tests/mfem_amgx/ex1p_amgx_agg_precon_l1_jacobi.sh num_proc_build=36  >> amgx_agg_precon_l1_jacobi.txt &&
+./go.sh -c lassen -m xlc -r tests/mfem_amgx/ex1p_amgx_agg_solver_l1_jacobi.sh num_proc_build=36  >> amgx_agg_solver_l1_jacobi.txt
